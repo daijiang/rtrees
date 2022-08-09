7e04421dc9761424ac5b1b5f251b5101e0041fe6
daijiang <daijianglee@gmail.com>
2022-08-09 17:18:22 -0500

    update classification doc

--- R/data.R
+++ R/data.R
@@ -19,8 +19,16 @@
 #' of BirdLife. Both are combined here, which leads to 2,508 genus.
 #' 
 #' - Mammal classification information was based on PHYLACINE, which has 1,400 genus. 
-#' <https://github.com/MegaPast2Future/PHYLACINE_1.2/blob/master/Data/Taxonomy/Synonymy_table_valid_species_only.csv>
+#' <https://github.com/MegaPast2Future/PHYLACINE_1.2/blob/master/Data/Taxonomy/Synonymy_table_valid_species_only.csv> Additional 
+#' genus from Vertlife were added too. For the same genus from both PHYLACINE and Vertlife that have different family information,
+#' I used the family from Vertlife as I found that they are mostly more accurate.
 #' 
-#' @format A data frame with three columns: genus, family, and taxon (`plant`, `fish`, `bird`, `mammal`). 
+#' - Amphibian classification information was from [VertLife](https://data.vertlife.org/amphibiantree/download/amph_shl_new_Classification.csv).
+#' 
+#' - Reptile classification information was largely from [wikipedia](https://en.wikipedia.org/wiki/List_of_reptile_genera).
+#' 
+#' - Shark and Ray classification information was largely from NCBI.
+#' 
+#' @format A data frame with three columns: genus, family, and taxon (`plant`, `fish`, `bird`, `mammal`, `amphibian`, `reptile`, `shark_ray`). 
 #' 
 "classifications"
